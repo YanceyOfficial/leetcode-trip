91a90eccc9ed98a88204414a1bcee7a3bdb6030a
YanceyOffical <yanceyofficial@gmail.com>
2019-10-27 19:09:17 +0800

    feat: learn LinkedList

--- dataStructures/LinkedList/LinkedList.ts
+++ dataStructures/LinkedList/LinkedList.ts
@@ -1,5 +1,24 @@
 import { Node } from './Node'
+import { congruentValue } from '../../shared/utils'
+import { ILinkedList } from './types'
 
-export class LinkedList {
-  constructor() {}
+export class LinkedList implements ILinkedList {
+  private count: number
+  private head: any
+  private equalsFn: Function
+
+  constructor(equalsFn = congruentValue) {
+    this.count = 0
+    this.head = undefined
+    this.equalsFn = equalsFn
+  }
+
+  public push(element: number) {
+    const node = new Node(element)
+
+    if (!this.head) {
+      // 如果此时链表为空
+
+    }
+  }
 }
