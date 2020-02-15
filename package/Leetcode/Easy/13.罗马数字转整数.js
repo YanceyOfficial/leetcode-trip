4a2a942fa8ef5055efcaf0b5fee7ea52fbdfc7a2
Yancey Leo <yanceyofficial@gmail.com>
2020-02-15 08:14:37 +0800

    feat: finish romanToInt

---
+++ package/Leetcode/Easy/13.罗马数字转整数.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=13 lang=javascript
+ *
+ * [13] 罗马数字转整数
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  const romaMap = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900,
+  }
+  const romaSplit = s.match(/(CM)|(CD)|(XC)|(XL)|(IX)|(IV)|(IX)|(\w)/g)
+  return romaSplit.reduce((acc, val) => acc + romaMap[val], 0)
+}
+// @lc code=end
