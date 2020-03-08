22a92be28405b12a0742119ae30b7709b024e1f7
Yancey Leo <yanceyofficial@gmail.com>
2020-03-08 23:04:01 +0800

    feat: finish twoSum

--- Leetcode/Easy/1.两数之和.js
+++ Leetcode/Easy/1.两数之和.js
@@ -12,12 +12,21 @@
  */
 var twoSum = function(nums, target) {
   const map = new Map()
-  for (let i = 0, l = nums.length; i < l; i += 1) {
+
+  for (let i = 0; i < nums.length; i += 1) {
     const dif = target - nums[i]
+
     if (map.has(dif)) {
       return [map.get(dif), i]
     }
+
     map.set(nums[i], i)
   }
 }
 // @lc code=end
+
+// 思路:
+//
+// 创建一个哈希表, 遍历数组, 将元素存储到哈希表中
+// 如果 target - nums[i] 存在于哈希表中
+// 那么就找到了这两个相加等于 target 的元素
