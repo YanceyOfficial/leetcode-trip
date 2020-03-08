ec8c5d76ca31869fc0a86c2b7f89405386279245
YanceyOfficial <yanceyofficial@gmail.com>
2020-03-08 10:27:40 +0800

    feat: finish countPrimes

--- Leetcode/Easy/541.反转字符串-ii.js
+++ Leetcode/Easy/541.反转字符串-ii.js
@@ -11,7 +11,17 @@
  * @return {string}
  */
 var reverseStr = function(s, k) {
+  let str = s.split('')
 
-};
+  let i = 0
+  let j = 1
+
+  while (j < s.length) {
+    ;[str[i], str[j]] = [str[j], str[i]]
+    i += 2 * k
+    j += 2 * k
+  }
+
+  return str.join('')
+}
 // @lc code=end
-
