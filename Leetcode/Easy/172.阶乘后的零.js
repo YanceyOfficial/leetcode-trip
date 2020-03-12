dadad3f4ba1c74b9e054c466cf51e554ae659200
Yancey Leo <yanceyofficial@gmail.com>
2020-03-13 01:51:03 +0800

    feat: update factorial

--- Leetcode/Easy/172.阶乘后的零.js
+++ Leetcode/Easy/172.阶乘后的零.js
@@ -11,7 +11,7 @@
  */
 var trailingZeroes = function(n) {
   let total = 0
-  
+
   while (n >= 5) {
     n = Math.floor(n / 5)
     total += n
@@ -19,3 +19,12 @@ var trailingZeroes = function(n) {
   return total
 }
 // @lc code=end
+
+// 阶乘:
+
+function factorial(n) {
+  if (n === 0) return 1
+  if (n === 1) return n
+
+  return n * factorial(n - 1)
+}
