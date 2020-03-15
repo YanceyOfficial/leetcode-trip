a47176bd9336ab735bb5359fc15b1debf2769e53
Yancey Leo <yanceyofficial@gmail.com>
2020-03-15 22:43:13 +0800

    feat: finish wordPattern

---
+++ LeetCode/Easy/290.单词规律.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=290 lang=javascript
+ *
+ * [290] 单词规律
+ */
+
+// @lc code=start
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, str) {
+  const strArr = str.split(' ')
+  const patternLen = pattern.length
+
+  if (strArr.length !== patternLen) return false
+
+  const map = {}
+  let i = 0
+
+  while (i < patternLen) {
+    const curr = map[pattern[i]]
+    
+    if (curr !== undefined) {
+      if (curr !== strArr[i]) return false
+
+      i++
+    } else {
+      if (Object.values(map).includes(strArr[i])) return false
+
+      map[pattern[i]] = strArr[i]
+      i++
+    }
+  }
+
+  return true
+}
+// @lc code=end
