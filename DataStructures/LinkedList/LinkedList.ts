826f76cd35216b26c32156e1d5f73d423da3bc20
Yancey Leo <yanceyofficial@gmail.com>
2020-03-16 20:57:50 +0800

    feat: update types of linkedlist

--- DataStructures/LinkedList/LinkedList.ts
+++ DataStructures/LinkedList/LinkedList.ts
@@ -1,7 +1,7 @@
 import { Node } from './Node'
 import { ILinkedList } from './types'
 
-export class LinkedList<T> implements ILinkedList {
+export class LinkedList<T> implements ILinkedList<T> {
   protected count: number
 
   protected head: Node<T> | null
@@ -13,13 +13,13 @@ export class LinkedList<T> implements ILinkedList {
 
   public push(element: T) {
     const node = new Node(element)
-    let current = null
+    let current: Node<T> | null = null
 
     // 如果链表为空, 即 head 为 null, 该节点将放置到链表头部
     if (this.isEmpty()) {
       this.head = node
     } else {
-      current = this.head
+      current = this.head as Node<T>
       // 否则一直遍历到链表的尾部
       while (current.next) {
         current = current.next
@@ -31,20 +31,20 @@ export class LinkedList<T> implements ILinkedList {
   }
 
   public removeAt(index: number) {
-    if (index < 0 || index > this.count - 1) return undefined
+    if (index < 0 || index > this.count - 1) return null
 
     let current = this.head
 
     // 如果删除第一个元素
     if (index === 0) {
-      this.head = current.next
+      this.head = (current as Node<T>).next as Node<T>
     } else {
       const previous = this.getElementAt(index - 1)
-      current = previous.next
-      previous.next = current.next
+      current = (previous as Node<T>).next as Node<T>
+      (previous as Node<T>).next = current.next
     }
     this.count--
-    return current.element
+    return (current as Node<T>).element
   }
 
   public remove(element: T) {
@@ -74,12 +74,12 @@ export class LinkedList<T> implements ILinkedList {
   }
 
   public getElementAt(index: number) {
-    // 如果索引越界, 返回 undefined
-    if (index < 0 || index > this.count - 1) return undefined
+    // 如果索引越界, 返回 null
+    if (index < 0 || index > this.count - 1) return null
 
-    let current = this.head
+    let current = this.head as Node<T>
     for (let i = 0; i < index; i += 1) {
-      current = current.next
+      current = current.next as Node<T>
     }
 
     return current
@@ -90,10 +90,11 @@ export class LinkedList<T> implements ILinkedList {
 
     // 遍历链表
     for (let i = 0; i < this.size(); i += 1) {
-      if (current.element === element) {
+      if ((current as Node<T>).element === element) {
         return i
       }
-      current = current.next
+
+      current = (current as Node<T>).next as Node<T>
     }
     return -1
   }
@@ -116,14 +117,14 @@ export class LinkedList<T> implements ILinkedList {
   }
 
   public toString() {
-    if (this.head == null) {
+    if (this.head === null) {
       return ''
     }
     let objString = `${this.head.element}`
     let current = this.head.next
     for (let i = 1; i < this.size() && current !== null; i++) {
-      objString = `${objString},${current.element}`
-      current = current.next
+      objString = `${objString},${(current as Node<T>).element}`
+      current = (current as Node<T>).next
     }
     return objString
   }
--- DataStructures/LinkedList/types.ts
+++ DataStructures/LinkedList/types.ts
@@ -1,13 +1,15 @@
-export interface ILinkedList {
-  push(element: any): void // 向链表尾部添加一个新元素
+import { Node } from './Node'
+
+export interface ILinkedList<T> {
+  push(element: T): void // 向链表尾部添加一个新元素
   removeAt(index: number): any // 删除指定位置的元素
-  remove(element: any): any // 从链表移除一个元素
-  insert(element: any, index: number): boolean // 向链表指定位置插入元素
-  getElementAt(index: number): any // 返回链表指定位置的元素, 若找不到返回 undefined
-  indexOf(element: any): number // 返回链表指定元素的索引, 没有则返回 -1
+  remove(element: T): any // 从链表移除一个元素
+  insert(element: T, index: number): boolean // 向链表指定位置插入元素
+  getElementAt(index: number): Node<T> | null // 返回链表指定位置的元素, 若找不到返回 null
+  indexOf(element: T): number // 返回链表指定元素的索引, 没有则返回 -1
   isEmpty(): boolean // 判断链表是否为空
   size(): number // 获取链表的长度
-  getHead(): any // 获取 head
-  clear(): any // 清空链表
+  getHead(): Node<T> | null // 获取 head
+  clear(): void // 清空链表
   toString(): string // 返回链表的字符串形式
 }
