9f1060d475e79950ba7cc071e30461144ce0cb56
Yancey Leo <yanceyofficial@gmail.com>
2020-03-21 22:48:21 +0800

    feat: update addTwoNumbers

--- LeetCode/Medium/2.两数相加.js
+++ LeetCode/Medium/2.两数相加.js
@@ -49,19 +49,3 @@ var addTwoNumbers = function(l1, l2) {
   return pre.next
 }
 // @lc code=end
-
-function ListNode(val) {
-  this.val = val
-  this.next = null
-}
-
-const l1 = new ListNode(2)
-l1.next = new ListNode(4)
-l1.next.next = new ListNode(3)
-
-const l2 = new ListNode(5)
-l2.next = new ListNode(6)
-l2.next.next = new ListNode(4)
-// l2.next.next.next = new ListNode(1)
-
-console.log(addTwoNumbers(l1, l2))
