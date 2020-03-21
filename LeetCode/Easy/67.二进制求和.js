546ddf8379d041ece0aca1603dde7e686f675488
Yancey Leo <yanceyofficial@gmail.com>
2020-03-21 23:26:50 +0800

    feat: update addBinary

--- LeetCode/Easy/67.二进制求和.js
+++ LeetCode/Easy/67.二进制求和.js
@@ -45,3 +45,9 @@ var addBinary = function(a, b) {
 // @lc code=end
 
 console.log(addBinary('1010', '1011'))
+
+// var // var addBinary = function(a, b) {
+//   return (BigInt('0b' + a) + BigInt('0b' + b)).toString(2);
+// }; = function(a, b) {
+//   return (BigInt('0b' + a) + BigInt('0b' + b)).toString(2);
+// };
