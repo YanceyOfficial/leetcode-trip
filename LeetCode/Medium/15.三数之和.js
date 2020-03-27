a127b7ab80ee642b42a0e9a4ac9f25cfa1de4c88
Yancey Leo <yanceyofficial@gmail.com>
2020-03-27 18:29:11 +0800

    feat: finish threeSum

--- LeetCode/Medium/15.三数之和.js
+++ LeetCode/Medium/15.三数之和.js
@@ -10,33 +10,27 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-  let res = []
-  let length = nums.length
-  nums.sort((a, b) => a - b) // 先排个队，最左边是最弱（小）的，最右边是最强(大)的
-  if (nums[0] <= 0 && nums[length - 1] >= 0) {
-    // 优化1: 整个数组同符号，则无解
-    for (let i = 0; i < length - 2; ) {
-      if (nums[i] > 0) break // 优化2: 最左值为正数则一定无解
-      let first = i + 1
-      let last = length - 1
-      do {
-        if (first >= last || nums[i] * nums[last] > 0) break // 两人选相遇，或者三人同符号，则退出
-        let result = nums[i] + nums[first] + nums[last]
-        if (result === 0) {
-          // 如果可以组队
-          res.push([nums[i], nums[first], nums[last]])
-        }
-        if (result <= 0) {
-          // 实力太弱，把菜鸟那边右移一位
-          while (first < last && nums[first] === nums[++first]) {} // 如果相等就跳过
-        } else {
-          // 实力太强，把大神那边右移一位
-          while (first < last && nums[last] === nums[--last]) {}
-        }
-      } while (first < last)
-      while (nums[i] === nums[++i]) {}
+  let ans = []
+  const len = nums.length
+  if (nums == null || len < 3) return ans
+  nums.sort((a, b) => a - b) // 排序
+  for (let i = 0; i < len; i++) {
+    if (nums[i] > 0) break // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+    if (i > 0 && nums[i] == nums[i - 1]) continue // 去重
+    let L = i + 1
+    let R = len - 1
+    while (L < R) {
+      const sum = nums[i] + nums[L] + nums[R]
+      if (sum == 0) {
+        ans.push([nums[i], nums[L], nums[R]])
+        while (L < R && nums[L] == nums[L + 1]) L++ // 去重
+        while (L < R && nums[R] == nums[R - 1]) R-- // 去重
+        L++
+        R--
+      } else if (sum < 0) L++
+      else if (sum > 0) R--
     }
   }
-  return res
+  return ans
 }
 // @lc code=end
