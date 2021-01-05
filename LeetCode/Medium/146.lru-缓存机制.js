9ea4664379857aa300072cddc3b5352d99b77c3e
liuyanzhao <liuyanzhao@MacintoshdeMacBook-Pro.local>
2021-01-05 11:30:32 +0800

    feat

--- LeetCode/Medium/146.lru-缓存机制.js
+++ LeetCode/Medium/146.lru-缓存机制.js
@@ -19,10 +19,15 @@ var LRUCache = function (capacity) {
  */
 LRUCache.prototype.get = function (key) {
   const val = this.caches.get(key)
+
+  // 如果不存在, 返回 -1
   if (val === undefined) return -1
 
+  // 如果存在, 因为用过一次了, 就把它删除掉, 重新 set 一次
   this.caches.delete(key)
   this.caches.set(key, val)
+
+  // 如果存在, 返回这个 val
   return val
 }
 
@@ -32,12 +37,15 @@ LRUCache.prototype.get = function (key) {
  * @return {void}
  */
 LRUCache.prototype.put = function (key, value) {
+  // 如果存在, 先把它删除
   if (this.caches.has(key)) {
     this.caches.delete(key)
   }
 
+  // 新增这个 k-v
   this.caches.set(key, value)
 
+  // 如果超过了容量, 就把 Map 最老的那个删除掉
   if (this.caches.size > this.capacity) {
     this.caches.delete(this.caches.keys().next().value)
   }
