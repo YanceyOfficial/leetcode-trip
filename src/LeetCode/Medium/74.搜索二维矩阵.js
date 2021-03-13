80bdba9962c4403b2ae53a5ebbd8ab5c437f9c85
Yancey Leo <yanceyofficial@gmail.com>
2021-03-13 16:04:23 +0800

    feat: add 74-search-matrix

--- src/LeetCode/Medium/74.搜索二维矩阵.js
+++ src/LeetCode/Medium/74.搜索二维矩阵.js
@@ -4,13 +4,25 @@
  * [74] 搜索二维矩阵
  */
 
+// 输入: `matrix = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]], target = 3`
+//
+// 输出: `true`
+
 // @lc code=start
 /**
  * @param {number[][]} matrix
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {
+var searchMatrix = function (matrix, target) {
+    const matrixLen = matrix.length
+    const matrixItemLen = matrix[0].length
 
-};
+  let left = 0
+  let right = matrix.length - 1
+
+  while(left <=right) {
+      const mid = 
+  }
+}
 // @lc code=end
