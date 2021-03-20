3b19a0fbabb0ddc0865cbe655110c6166df5204e
Yancey Leo <yanceyofficial@gmail.com>
2021-03-21 00:38:17 +0800

    feat: support mdx

--- sidebarsLeetcode.js
+++ sidebarsLeetcode.js
@@ -3,7 +3,7 @@ const { readdirSync } = require('fs')
 const getFileList = (category) =>
   readdirSync(`./leetcode-docs/${category}`)
     .sort((a, b) => +a.split('-')[0] - +b.split('-')[0])
-    .map((fileName) => `${category}/${fileName.replace('.md', '')}`)
+    .map((fileName) => `${category}/${fileName.replace(/.mdx?/, '')}`)
 
 module.exports = {
   leetcode: {
