d4a69209d4a6dddcd1cd5375ad2c6511ba4e23a9
Yancey Leo <yanceyofficial@gmail.com>
2021-03-28 16:16:49 +0800

    chore(babel-preset): delete babel-preset dependency

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
-    {
-      targets: {
-        node: 'current',
-      },
-    },
     require.resolve('@docusaurus/core/lib/babel/preset'),
     '@babel/preset-typescript',
   ],
