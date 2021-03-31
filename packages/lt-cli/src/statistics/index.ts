a505a6b910177fee7d07c23322f3f14e472cb9a8
Yancey Leo <yanceyofficial@gmail.com>
2021-03-31 13:18:08 +0800

    feat: update lt-cli

--- packages/lt-cli/src/statistics/index.ts
+++ packages/lt-cli/src/statistics/index.ts
@@ -4,12 +4,13 @@ import { srcPath, Category } from '../shared/constants'
 const easyCount = readdirSync(`${srcPath}/${Category.Easy}`).length
 const mediumCount = readdirSync(`${srcPath}/${Category.Medium}`).length
 const hardCount = readdirSync(`${srcPath}/${Category.Hard}`).length
+const totcal = easyCount + mediumCount + hardCount
 
 const statistics = [
   { category: Category.Easy, conut: easyCount },
   { category: Category.Medium, conut: mediumCount },
   { category: Category.Hard, conut: hardCount },
-  { category: 'Total', conut: easyCount + mediumCount + hardCount },
+  { category: 'Total', conut: totcal },
 ]
 
 console.table(statistics)
