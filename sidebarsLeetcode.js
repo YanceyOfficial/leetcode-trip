7957133bb24abcb0719aa53df2301a95583dadc8
Yancey Leo <yanceyofficial@gmail.com>
2021-04-11 00:05:32 +0800

    feat: update sidebarsLeetcode.js

--- sidebarsLeetcode.js
+++ sidebarsLeetcode.js
@@ -1,15 +1,15 @@
 const { readdirSync } = require('fs')
 
-const getFileList = (category) =>
+const getFiles = (category) =>
   readdirSync(`./leetcode-docs/${category}`)
     .sort((a, b) => +a.split('-')[0] - +b.split('-')[0])
     .map((fileName) => `${category}/${fileName.replace(/.mdx?/, '')}`)
 
 module.exports = {
   leetcode: {
-    Easy: getFileList('easy'),
-    Medium: getFileList('medium'),
-    Hard: getFileList('hard'),
-    Others: getFileList('others'),
+    Easy: getFiles('easy'),
+    Medium: getFiles('medium'),
+    Hard: getFiles('hard'),
+    Others: getFiles('others'),
   },
 }
---
+++ src/LeetCode/Easy/804.唯一摩尔斯密码词.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=804 lang=javascript
+ *
+ * [804] 唯一摩尔斯密码词
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+
+};
+// @lc code=end
+
