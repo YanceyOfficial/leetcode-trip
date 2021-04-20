50391d8b7bb06c399c9da876197df52a1c501818
Yancey Leo <yanceyofficial@gmail.com>
2021-04-20 10:00:54 +0800

    feat: add 319-bulb-switch

--- src/leetcode/Easy/268.缺失数字.js
+++ src/leetcode/Easy/268.缺失数字.js
@@ -9,10 +9,12 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
+var missingNumber = function (nums) {
   const len = nums.length
   const total = ((1 + len) * len) / 2
 
   return total - nums.reduce((acc, val) => acc + val)
 }
 // @lc code=end
+
+// 异或运算. https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247484477&idx=1&sn=13834cfd618377385226c3dc598b2c28&chksm=9bd7fa35aca0732374dc34c78c276b982605892caf69cb31ad9a6c3685de5dbccac81989b195&scene=21#wechat_redirect
---
+++ src/leetcode/Medium/877.石子游戏.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=877 lang=javascript
+ *
+ * [877] 石子游戏
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function (piles) {
+  return true
+}
+// @lc code=end
