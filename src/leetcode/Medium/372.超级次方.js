7507036e3b2eea4441279422d7e2577f03bbd710
Yancey Leo <yanceyofficial@gmail.com>
2021-04-20 21:02:56 +0800

    feat: [WIP] add 372-super-pow

---
+++ src/leetcode/Medium/372.超级次方.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=372 lang=javascript
+ *
+ * [372] 超级次方
+ */
+
+// @lc code=start
+/**
+ * @param {number} a
+ * @param {number[]} b
+ * @return {number}
+ */
+var superPow = function (a, b) {}
+// @lc code=end
