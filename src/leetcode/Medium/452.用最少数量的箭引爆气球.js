2deed4cb2ef013ebf3c4660f636ccc444a7c7699
Yancey Leo <yanceyofficial@gmail.com>
2021-04-23 09:21:38 +0800

    feat: [WIP] 452-find-min-arrow-shots

---
+++ src/leetcode/Medium/452.用最少数量的箭引爆气球.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=452 lang=javascript
+ *
+ * [452] 用最少数量的箭引爆气球
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var findMinArrowShots = function (points) {}
+// @lc code=end
