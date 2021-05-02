a4cec840dbef5bd01cc4ca9ea2bd77ae895f6a84
YanceyOfficial <yanceyofficial@gmail.com>
2021-05-03 00:34:30 +0800

    feat: add 41-first-missing-positive

--- leetcode-docs/hard/41-first-missing-positive.md
+++ leetcode-docs/hard/41-first-missing-positive.md
@@ -57,3 +57,30 @@ var firstMissingPositive = function (nums) {
   return min
 }
 ```
+
+### 原地哈希
+
+即使用数字下标作为哈希.
+
+```ts
+var firstMissingPositive = function (nums) {
+  const n = nums.length
+
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      // 满足在指定范围内, 并且没有放在正确的位置上, 才交换
+      // 例如: 数值 3 应该放在索引 2 的位置上
+      ;[nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]]
+    }
+  }
+
+  // [1, -1, 3, 4]
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) {
+      return i + 1
+    }
+  }
+  // 都正确则返回数组长度 + 1
+  return n + 1
+}
+```
--- src/leetcode/Hard/41.缺失的第一个正数.js
+++ src/leetcode/Hard/41.缺失的第一个正数.js
@@ -11,21 +11,23 @@
  */
 var firstMissingPositive = function (nums) {
   const n = nums.length
-  let min = 1
 
-  nums.sort((a, b) => a - b)
-
-  for (const num of nums) {
-    if (num === min) {
-      min += 1
-      continue
-    }
-    if (num > min) {
-      break
+  for (let i = 0; i < n; i++) {
+    while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] !== nums[i]) {
+      // 满足在指定范围内, 并且没有放在正确的位置上, 才交换
+      // 例如: 数值 3 应该放在索引 2 的位置上
+      ;[nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]]
     }
   }
 
-  return min
+  // [1, -1, 3, 4]
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) {
+      return i + 1
+    }
+  }
+  // 都正确则返回数组长度 + 1
+  return n + 1
 }
 // @lc code=end
 console.log(firstMissingPositive([3, 4, -1, 1]))
