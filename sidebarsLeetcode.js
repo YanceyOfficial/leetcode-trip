b05aa56a0a209330298fa23941d7bf0c82afafe2
YanceyOfficial <yanceyofficial@gmail.com>
2022-10-03 21:19:41 +0800

    feat: add 3-count-bits

--- sidebarsLeetcode.js
+++ sidebarsLeetcode.js
@@ -11,6 +11,7 @@ module.exports = {
     Medium: getFiles('medium'),
     Hard: getFiles('hard'),
     '剑指 Offer': getFiles('lcof'),
+    '剑指 Offer 专项突击版': getFiles('lcof-v2'),
     Others: getFiles('others'),
   },
 }
