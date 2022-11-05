24c7f5f98bfb829f0e52a2438c7d4bcad01d4b93
YanceyOfficial <yanceyofficial@gmail.com>
2022-11-05 18:48:08 +0800

    feat: resolve 814-prune-tree

---
+++ src/leetcode/javascript/medium/814.二叉树剪枝.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=814 lang=javascript
+ *
+ * [814] 二叉树剪枝
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var pruneTree = function(root) {
+
+};
+// @lc code=end
+
