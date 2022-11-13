641f0c755f5b394ed646c0e2352a18daab8b7c1a
YanceyOfficial <yanceyofficial@gmail.com>
2022-11-13 15:55:35 +0800

    feat: resolve 35-search-insert in rust

--- src/leetcode/rust/src/easy/question_35.rs
+++ src/leetcode/rust/src/easy/question_35.rs
@@ -1,3 +1,4 @@
+#[allow(unused)]
 pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
     let (mut low, mut high) = (0, nums.len() - 1);
 
@@ -9,7 +10,6 @@ pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
         } else if nums[mid] < target {
             low = mid + 1;
         } else {
-            
             if mid == 0 {
                 return mid as i32;
             }
