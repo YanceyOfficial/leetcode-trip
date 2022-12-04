e5652048f64841e548b04698550e2625a8aa6999
YanceyOfficial <yanceyofficial@gmail.com>
2022-12-05 00:27:39 +0800

    feat: resolve 198-rob

--- src/leetcode/javascript/medium/198.打家劫舍.js
+++ src/leetcode/javascript/medium/198.打家劫舍.js
@@ -10,16 +10,21 @@
  * @return {number}
  */
 var rob = function (nums) {
+  const n = nums.length
+  if (n === 0) return 0
+  if (n === 1) return nums[0]
+
   let rob = 0,
     notRob = 0
-  const n = nums.length
 
   for (let i = 0; i < n; i++) {
-    let preRob = rob,
-      preNotRob = notRob
-    rob = preNotRob + nums[i]
-    notRob = Math.max(preRob, preNotRob)
+    let temp = rob
+    // 如果抢当前的, 那么前一个一定不能抢
+    rob = notRob + nums[i]
+    // 如果不抢当前的, 那么前一个可抢可不抢, 选价值最高的即可
+    notRob = Math.max(temp, notRob)
   }
+
   return Math.max(rob, notRob)
 }
 // @lc code=end
--- src/leetcode/rust/src/medium/question_198.rs
+++ src/leetcode/rust/src/medium/question_198.rs
@@ -2,10 +2,18 @@ use std::cmp;
 
 #[allow(unused)]
 pub fn rob(nums: Vec<i32>) -> i32 {
+    let n = nums.len();
+    if n == 0 {
+        return 0;
+    };
+    if n == 1 {
+        return nums[0];
+    };
+
     let mut rob = 0;
     let mut not_rob = 0;
 
-    for i in 0..nums.len() {
+    for i in 0..n {
         let temp = rob;
         rob = not_rob + nums[i];
         not_rob = cmp::max(temp, not_rob);
