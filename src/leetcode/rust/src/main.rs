f7e5f42aa0a768e1447b1371e22f848785ec9361
YanceyOfficial <yanceyofficial@gmail.com>
2022-12-20 16:56:22 +0800

    feat: resolve 72-min-distance

--- src/leetcode/rust/src/main.rs
+++ src/leetcode/rust/src/main.rs
@@ -3,10 +3,9 @@ mod hard;
 mod medium;
 
 fn main() {
-    let res = medium::question_97::is_interleave(
-        String::from("db"),
-        String::from("b"),
-        String::from("cbb"),
+    let res = hard::question_72::min_distance(
+        String::from("horse"),
+        String::from("ros"),
     );
 
     println!("{:?}", res);
