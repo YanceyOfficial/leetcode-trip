c73f40d6b959f7ee49d34471a8db99a7b1d6d980
YanceyOfficial <yanceyofficial@gmail.com>
2022-12-20 16:56:36 +0800

    feat: resolve 72-min-distance

--- src/leetcode/javascript/hard/72.编辑距离.js
+++ src/leetcode/javascript/hard/72.编辑距离.js
@@ -10,5 +10,25 @@
  * @param {string} word2
  * @return {number}
  */
-var minDistance = function (word1, word2) {}
+var minDistance = function (word1, word2) {
+  const m = word1.length
+  const n = word2.length
+  const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0))
+
+  for (let i = 0; i <= m; i++) dp[i][0] = i
+  for (let j = 0; j <= n; j++) dp[0][j] = j
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1]
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+      }
+    }
+  }
+
+  return dp[m][n]
+}
 // @lc code=end
+minDistance('horse', 'ros')
