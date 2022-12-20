6b13ba6c57bf2fec226ffbf414e3f280e8cd7f0f
YanceyOfficial <yanceyofficial@gmail.com>
2022-12-20 11:29:27 +0800

    feat(wip): 72-min-distance

---
+++ src/leetcode/javascript/hard/72.编辑距离.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode.cn id=72 lang=javascript
+ *
+ * [72] 编辑距离
+ */
+
+// @lc code=start
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {}
+// @lc code=end
--- src/leetcode/rust/src/hard/mod.rs
+++ src/leetcode/rust/src/hard/mod.rs
@@ -1,5 +1,6 @@
 pub mod question_115;
 pub mod question_132;
+pub mod question_72;
 pub mod question_76;
 pub mod question_84;
 pub mod question_85;
