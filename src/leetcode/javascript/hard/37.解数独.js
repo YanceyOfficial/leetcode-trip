e7a216f3de5b8867df9a13d6f1f2c187e408dd86
YanceyOfficial <yanceyofficial@gmail.com>
2023-01-14 22:23:57 +0800

    feat: resolve 37-solve-sudoku

---
+++ src/leetcode/javascript/hard/37.解数独.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=37 lang=javascript
+ *
+ * [37] 解数独
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solveSudoku = function (board) {
+  const isValid = (ch, row, col) => {
+    for (let i = 0; i < 9; i++) {
+      if (board[row][i] === ch) return false
+      if (board[i][col] === ch) return false
+      if (
+        board[((row / 3) | 0) * 3 + ((i / 3) | 0)][
+          ((col / 3) | 0) * 3 + (i % 3)
+        ] === ch
+      )
+        return false
+    }
+    return true
+  }
+
+  const backtrack = (row, col) => {
+    if (col === 9) return backtrack(row + 1, 0)
+    if (row === 9) return true
+
+    for (let i = row; i < 9; i++) {
+      for (let j = col; j < 9; j++) {
+        if (board[i][j] !== '.') {
+          return backtrack(i, j + 1)
+        }
+
+        for (let k = 1; k <= 9; k++) {
+          if (!isValid(k.toString(), i, j)) continue
+
+          board[i][j] = k.toString()
+          if (backtrack(i, j + 1)) return true
+          board[i][j] = '.'
+        }
+
+        return false
+      }
+    }
+
+    return true
+  }
+
+  backtrack(0, 0)
+}
+// @lc code=end
---
+++ src/leetcode/rust/src/hard/question_37.rs
@@ -0,0 +1,59 @@
+#[allow(unused)]
+pub fn solve_sudoku(board: &mut Vec<Vec<char>>) {
+    backtrack(board, 0, 0);
+}
+
+fn backtrack(board: &mut Vec<Vec<char>>, row: usize, col: usize) -> bool {
+    if col == 9 {
+        return backtrack(board, row + 1, 0);
+    }
+
+    if row == 9 {
+        return true;
+    }
+
+    for i in row..9 {
+        for j in col..9 {
+            if board[i][j] != '.' {
+                return backtrack(board, i, j + 1);
+            }
+
+            for ch in '1'..='9' {
+                if !is_valid(board, row, col, ch) {
+                    continue;
+                }
+
+                board[i][j] = ch;
+                if backtrack(board, i, j + 1) {
+                    return true;
+                }
+                board[i][j] = '.';
+            }
+
+            return false;
+        }
+    }
+
+    return true;
+}
+
+fn is_valid(board: &Vec<Vec<char>>, row: usize, col: usize, ch: char) -> bool {
+    let ret: Result<i32, std::io::Error> = Ok(1);
+    if let Err(_) = ret {}
+
+    for i in 0..9 {
+        if board[row][i] == ch {
+            return false;
+        }
+
+        if board[i][col] == ch {
+            return false;
+        }
+
+        if board[row / 3 * 3 + (i / 3)][col / 3 * 3 + (i % 3)] == ch {
+            return false;
+        }
+    }
+
+    return true;
+}
