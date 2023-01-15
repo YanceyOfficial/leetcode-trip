6bcc029930b7d06e57d397a61f41f237e69daa21
YanceyOfficial <yanceyofficial@gmail.com>
2023-01-15 22:12:57 +0800

    feat: resolve 14-longest-common-prefix in rust

--- src/leetcode/javascript/easy/14.最长公共前缀.js
+++ src/leetcode/javascript/easy/14.最长公共前缀.js
@@ -9,17 +9,14 @@
  * @param {string[]} strs
  * @return {string}
  */
-
 var longestCommonPrefix = function (strs) {
-  if (strs.length === 0) return ''
-
   let prefix = strs[0]
-  let flag = false
+  let hasFound = false
 
-  while (!flag) {
-    flag = strs.every((val) => val.indexOf(prefix) === 0)
+  while (!hasFound) {
+    hasFound = strs.every((str) => str.startsWith(prefix))
 
-    if (!flag) {
+    if (!hasFound) {
       prefix = prefix.slice(0, -1)
     }
   }
@@ -27,11 +24,3 @@ var longestCommonPrefix = function (strs) {
   return prefix
 }
 // @lc code=end
-
-// 思路
-//
-// 将数组第一个元素设为 prefix
-// 声明一个 Boolean 类型的变量 flag, 用于判断是否已经匹配到 LCP
-// 在 while 循环中, 使用数组的 every 方法判断 prefix 是否为公共子串
-// 如果是的将 flag 设为 true, 跳出循环, 返回 prefix
-// 否则将 prefix 赋值成 prefix.slice(0, -1) 继续循环
---
+++ src/leetcode/rust/src/easy/question_14.rs
@@ -0,0 +1,15 @@
+#[allow(unused)]
+pub fn longest_common_prefix(strs: Vec<String>) -> String {
+    let mut prefix: &str = &strs[0];
+    let mut has_found = false;
+
+    while !has_found {
+        has_found = strs.iter().all(|str| str.starts_with(prefix));
+
+        if !has_found {
+            prefix = &prefix[0..prefix.len() - 1];
+        }
+    }
+
+    prefix.to_string()
+}
