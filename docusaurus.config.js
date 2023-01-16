2c29b09b0344269a1128c076dd1da8960b4d1749
YanceyOfficial <yanceyofficial@gmail.com>
2023-01-16 11:40:03 +0800

    feat: remove comments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,28 +13,6 @@ module.exports = {
   organizationName: 'Yancey Inc.',
   projectName: 'leetcode-trip',
   themes: ['@docusaurus/theme-live-codeblock'],
-  scripts: [
-    {
-      src: 'https://giscus.app/client.js',
-      'data-repo': 'YanceyOfficial/leetcode-trip',
-      'data-repo-id': 'MDEwOlJlcG9zaXRvcnkxODkzNTcxMDg=',
-      'data-category': 'Q&A',
-      'data-category-id': 'DIC_kwDOC0lcNM4CS6Dj',
-      'data-mapping': 'title',
-      'data-strict': '0',
-      'data-reactions-enabled': '1',
-      'data-emit-metadata': '0',
-      'data-input-position': 'bottom',
-      'data-theme': 'preferred_color_scheme',
-      'data-lang': 'zh-CN',
-      crossorigin: 'anonymous',
-      async: true,
-    },
-    {
-      src: '/js/giscus.js',
-      defer: true,
-    },
-  ],
   plugins: [
     [
       '@docusaurus/plugin-content-docs',
