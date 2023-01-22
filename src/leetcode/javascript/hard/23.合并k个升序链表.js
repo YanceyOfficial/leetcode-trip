683c0128028220f2e998c1457d5fedb3932d5e4d
YanceyOfficial <yanceyofficial@gmail.com>
2023-01-23 02:15:10 +0800

    feat: update 21-merge-two-lists

--- src/leetcode/javascript/hard/23.合并k个升序链表.js
+++ src/leetcode/javascript/hard/23.合并k个升序链表.js
@@ -18,32 +18,41 @@
  */
 var mergeKLists = function (lists) {
   let n = lists.length
+
   if (n === 0) return null
+
   while (n > 1) {
-    const k = Math.floor((n + 1) / 2)
-    for (let i = 0; i < Math.floor(n / 2); ++i) {
+    const k = ((n + 1) / 2) | 0
+
+    for (let i = 0; i < ((n / 2) | 0); i++) {
       lists[i] = mergeTwoLists(lists[i], lists[i + k])
     }
+
     n = k
   }
+
   return lists[0]
 }
 
 var mergeTwoLists = function (l1, l2) {
   const dummy = new ListNode(-1)
-  let cur = dummy
+  let curr = dummy
+
   while (l1 && l2) {
     if (l1.val < l2.val) {
-      cur.next = l1
+      curr.next = l1
       l1 = l1.next
     } else {
-      cur.next = l2
+      curr.next = l2
       l2 = l2.next
     }
-    cur = cur.next
+
+    curr = curr.next
   }
-  if (l1) cur.next = l1
-  if (l2) cur.next = l2
+
+  if (l1 !== null) curr.next = l1
+  if (l2 !== null) curr.next = l2
+
   return dummy.next
 }
 // @lc code=end
