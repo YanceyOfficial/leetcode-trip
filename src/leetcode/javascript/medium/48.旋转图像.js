d8c84bccb4231bf253cd763dc413fe1d1f9872a3
YanceyOfficial <yanceyofficial@gmail.com>
2023-02-17 14:08:12 +0800

    feat: resolve 48-rotate

--- src/leetcode/javascript/medium/48.旋转图像.js
+++ src/leetcode/javascript/medium/48.旋转图像.js
@@ -9,5 +9,17 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var rotate = function (matrix) {}
+var rotate = function (matrix) {
+  const n = matrix.length
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      ;[matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    matrix[i].reverse()
+  }
+}
 // @lc code=end
--- src/leetcode/rust/src/medium/question_48.rs
+++ src/leetcode/rust/src/medium/question_48.rs
@@ -1,2 +1,16 @@
 #[allow(unused)]
-pub fn rotate(matrix: &mut Vec<Vec<i32>>) {}
+pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+    let n = matrix.len();
+
+    for i in 0..n {
+        for j in i..n {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for i in 0..n {
+        matrix[i].reverse();
+    }
+}
