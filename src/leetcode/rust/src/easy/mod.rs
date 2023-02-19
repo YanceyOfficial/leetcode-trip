84c521f0df8124a6fe5ffe5abbdd131c41eb9c5c
YanceyOfficial <yanceyofficial@gmail.com>
2023-02-19 16:06:18 +0800

    feat(wip): add template for 58-length-of-last-word

--- src/leetcode/rust/src/easy/mod.rs
+++ src/leetcode/rust/src/easy/mod.rs
@@ -10,6 +10,7 @@ pub mod question_27;
 pub mod question_346;
 pub mod question_35;
 pub mod question_496;
+pub mod question_58;
 pub mod question_680;
 pub mod question_69;
 pub mod question_7;
---
+++ src/leetcode/rust/src/easy/question_58.rs
@@ -0,0 +1,2 @@
+#[allow(unused)]
+pub fn length_of_last_word(s: String) -> i32 {}
