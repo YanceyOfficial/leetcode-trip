9de4f7815f1da9d7b97da81b51caf81a7855394b
YanceyOfficial <yanceyofficial@gmail.com>
2023-02-21 13:46:00 +0800

    feat: resolve 54-spiral-order in rust

--- src/leetcode/rust/src/main.rs
+++ src/leetcode/rust/src/main.rs
@@ -3,7 +3,11 @@ mod hard;
 mod medium;
 
 fn main() {
-    let res = hard::question_52::total_n_queens(4);
+    let res = medium::question_54::spiral_order(vec![
+        vec![1, 2, 3, 4],
+        vec![5, 6, 7, 8],
+        vec![9, 10, 11, 12],
+    ]);
 
     println!("{:?}", res);
 }
--- src/leetcode/rust/src/medium/question_54.rs
+++ src/leetcode/rust/src/medium/question_54.rs
@@ -1,4 +1,39 @@
 #[allow(unused)]
 pub fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
-    todo!()
+    let (mut top, mut right, mut bottom, mut left) = (0, matrix[0].len() - 1, matrix.len() - 1, 0);
+    let mut res = vec![];
+
+    while left <= right && top <= bottom {
+        for i in left..=right {
+            res.push(matrix[top][i]);
+        }
+        top += 1;
+
+        for i in top..=bottom {
+            res.push(matrix[i][right]);
+        }
+        if right == 0 {
+            break;
+        }
+        right -= 1;
+
+        if left > right || top > bottom {
+            break;
+        }
+
+        for i in (left..=right).rev() {
+            res.push(matrix[bottom][i]);
+        }
+        if bottom == 0 {
+            break;
+        }
+        bottom -= 1;
+
+        for i in (top..=bottom).rev() {
+            res.push(matrix[i][left]);
+        }
+        left += 1;
+    }
+
+    res
 }
