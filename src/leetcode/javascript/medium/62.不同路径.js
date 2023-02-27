02d897c0a1f1fcf27f38194b50b254e98df5195b
YanceyOfficial <yanceyofficial@gmail.com>
2023-02-27 11:12:34 +0800

    feat: add js comments for 62-unique-paths

--- src/leetcode/javascript/medium/62.不同路径.js
+++ src/leetcode/javascript/medium/62.不同路径.js
@@ -10,33 +10,13 @@
  * @param {number} n
  * @return {number}
  */
-
-// var uniquePaths = function (m, n) {
-//   const dp = new Array(n).fill(1)
-//   for (let i = 1; i < m; i++) {
-//     for (let j = 1; j < n; j++) {
-//       dp[j] = dp[j - 1] + dp[j]
-//     }
-//   }
-//   return dp[n - 1]
-// }
-// @lc code=end
-
 var uniquePaths = function (m, n) {
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0))
-
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1
-  }
-
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = 1
-  }
-
+  const dp = new Array(n).fill(1)
   for (let i = 1; i < m; i++) {
     for (let j = 1; j < n; j++) {
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+      dp[j] = dp[j - 1] + dp[j]
     }
   }
-  return dp[m - 1][n - 1]
+  return dp[n - 1]
 }
+// @lc code=end
