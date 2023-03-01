659f4841ca0d41bded1839151d45a249b5c08d5f
YanceyOfficial <yanceyofficial@gmail.com>
2023-03-01 15:56:52 +0800

    feat: update 64-min-path-sum

--- src/leetcode/rust/src/medium/question_64.rs
+++ src/leetcode/rust/src/medium/question_64.rs
@@ -2,25 +2,25 @@ use std::cmp;
 
 #[allow(unused)]
 pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
-    let rows = grid.len();
-    let columns = grid[0].len();
+    let m = grid.len();
+    let n = grid[0].len();
 
-    let mut dp = vec![vec![0; columns]; rows];
+    let mut dp = vec![vec![0; n]; m];
     dp[0][0] = grid[0][0];
 
-    for i in 1..rows {
+    for i in 1..m {
         dp[i][0] = dp[i - 1][0] + grid[i][0];
     }
 
-    for j in 1..columns {
+    for j in 1..n {
         dp[0][j] = dp[0][j - 1] + grid[0][j];
     }
 
-    for i in 1..rows {
-        for j in 1..columns {
+    for i in 1..m {
+        for j in 1..n {
             dp[i][j] = cmp::min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
         }
     }
 
-    dp[rows - 1][columns - 1]
+    dp[m - 1][n - 1]
 }
