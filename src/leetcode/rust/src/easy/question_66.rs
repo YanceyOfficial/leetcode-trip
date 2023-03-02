6155834f73b4d9e2aaf7ae7dc99512fed80489fc
YanceyOfficial <yanceyofficial@gmail.com>
2023-03-02 20:24:14 +0800

    feat: resolve 66-plus-one in rust

--- src/leetcode/rust/src/easy/question_66.rs
+++ src/leetcode/rust/src/easy/question_66.rs
@@ -1,4 +1,20 @@
 #[allow(unused)]
 pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
-    todo!();
+    let n = digits.len();
+    let mut digits = digits;
+
+    for i in (0..n).rev() {
+        if digits[i] == 9 {
+            digits[i] = 0;
+
+            if i == 0 {
+                digits.insert(0, 1);
+            }
+        } else {
+            digits[i] += 1;
+            break;
+        }
+    }
+
+    digits
 }
