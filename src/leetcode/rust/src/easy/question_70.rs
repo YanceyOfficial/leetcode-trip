18a9756d77ae19b2260c30f467b7f8990cf81b53
YanceyOfficial <yanceyofficial@gmail.com>
2023-03-04 20:45:19 +0800

    feat: resolve 70-climb-stairs in rust

--- src/leetcode/rust/src/easy/question_70.rs
+++ src/leetcode/rust/src/easy/question_70.rs
@@ -1,4 +1,13 @@
 #[allow(unused)]
 pub fn climb_stairs(n: i32) -> i32 {
-    todo!();
+    let mut prev = 1;
+    let mut curr = 1;
+
+    for i in 2..=(n as usize) {
+        let sum = prev + curr;
+        prev = curr;
+        curr = sum;
+    }
+
+    curr
 }
