d40973556eced05f92633ccf5bd31bbeaaf8125d
YanceyOfficial <yanceyofficial@gmail.com>
2023-03-09 11:48:59 +0800

    feat: resolve 74-search-matrix in rust

--- src/leetcode/rust/src/medium/question_74.rs
+++ src/leetcode/rust/src/medium/question_74.rs
@@ -1,4 +1,24 @@
 #[allow(unused)]
 pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-    !todo!();
+    let (m, n) = (matrix.len(), matrix[0].len());
+    let (mut left, mut right) = (0, m * n - 1);
+
+    while left <= right {
+        let mid = (left + right) / 2;
+        let mid_num = matrix[mid / n][mid % n];
+
+        if (mid_num < target) {
+            left = mid + 1;
+        } else if mid_num > target {
+            if (mid == 0) {
+                break;
+            }
+
+            right = mid - 1;
+        } else {
+            return true;
+        }
+    }
+
+    return false;
 }
