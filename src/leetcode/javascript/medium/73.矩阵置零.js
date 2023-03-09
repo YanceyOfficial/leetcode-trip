5292b9fadbfe25a72067172a57709185be2085ae
YanceyOfficial <yanceyofficial@gmail.com>
2023-03-09 10:47:01 +0800

    feat(wip): 73-set-zeroes

--- src/leetcode/javascript/medium/73.矩阵置零.js
+++ src/leetcode/javascript/medium/73.矩阵置零.js
@@ -9,5 +9,23 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-var setZeroes = function (matrix) {}
+var setZeroes = function (matrix) {
+  const m = matrix.length
+  const n = matrix[0].length
+
+  let flagCol0 = false
+}
 // @lc code=end
+const matrix = [
+  [0, 1, 2, 0],
+  [3, 4, 5, 2],
+  [1, 3, 1, 5],
+]
+setZeroes(matrix)
+console.log(matrix)
+
+const _matrix = [
+  [0, 0, 0, 0],
+  [0, 4, 5, 0],
+  [0, 3, 1, 0],
+]
