24e6216edb950cfee148920e56b77f6b87f4aaa7
YanceyOfficial <yanceyofficial@gmail.com>
2023-03-13 00:35:05 +0800

    feat: add solved count for sidebar

--- sidebars/sidebarsLeetcode.js
+++ sidebars/sidebarsLeetcode.js
@@ -5,12 +5,18 @@ const getFiles = (category) =>
     .sort((a, b) => +a.split('-')[0] - +b.split('-')[0])
     .map((fileName) => `${category}/${fileName.replace(/.mdx?/, '')}`)
 
+const easy = getFiles('easy')
+const medium = getFiles('medium')
+const hard = getFiles('hard')
+const lcof = getFiles('lcof')
+const others = getFiles('others')
+
 module.exports = {
   leetcode: {
-    Easy: getFiles('easy'),
-    Medium: getFiles('medium'),
-    Hard: getFiles('hard'),
-    '剑指 Offer': getFiles('lcof'),
-    Others: getFiles('others'),
+    [`Easy (${easy.length})`]: easy,
+    [`Medium (${medium.length})`]: medium,
+    [`Hard (${hard.length})`]: hard,
+    [`剑指 Offer (${lcof.length})`]: lcof,
+    [`Others (${others.length})`]: others,
   },
 }
