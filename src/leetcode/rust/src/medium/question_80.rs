657ecc58ef6b9c374ec64c2225a766d661cf32ee
YanceyOfficial <yanceyofficial@gmail.com>
2023-03-15 00:24:58 +0800

    feat: solve remove-duplicates by general solution

--- src/leetcode/rust/src/medium/question_80.rs
+++ src/leetcode/rust/src/medium/question_80.rs
@@ -1,11 +1,13 @@
 #[allow(unused)]
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
     let n = nums.len();
+    if n <= 2 {
+        return n as i32;
+    }
 
-    let (mut slow, mut fast) = (1, 1);
-
+    let (mut slow, mut fast) = (2, 2);
     while fast < n {
-        if nums[slow - 1] != nums[fast] {
+        if nums[slow - 2] != nums[fast] {
             nums[slow] = nums[fast];
             slow += 1;
         }
