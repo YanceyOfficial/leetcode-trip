427427f600e298bfadbba7a556f70e360ab25a84
YanceyOfficial <yanceyofficial@gmail.com>
2023-03-21 17:34:01 +0800

    feat: solve 89-gray-code

--- src/leetcode/javascript/medium/89.格雷编码.js
+++ src/leetcode/javascript/medium/89.格雷编码.js
@@ -9,5 +9,17 @@
  * @param {number} n
  * @return {number[]}
  */
-var grayCode = function (n) {}
+var grayCode = function (n) {
+  const res = [0]
+
+  for (let i = 1; i <= n; i++) {
+    const m = res.length
+
+    for (let j = m - 1; j >= 0; j--) {
+      res.push(res[j] | (1 << (i - 1)))
+    }
+  }
+
+  return res
+}
 // @lc code=end
--- src/leetcode/rust/src/medium/question_89.rs
+++ src/leetcode/rust/src/medium/question_89.rs
@@ -1,4 +1,14 @@
 #[allow(unused)]
 pub fn gray_code(n: i32) -> Vec<i32> {
-    !todo!();
+    let mut res = vec![0];
+
+    for i in 1..=n {
+        let m = res.len();
+
+        for j in (0..m).rev() {
+            res.push(res[j] | (1 << (i - 1)));
+        }
+    }
+
+    res
 }
