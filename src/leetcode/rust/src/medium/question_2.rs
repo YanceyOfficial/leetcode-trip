aa3c9a588a5b391fed030ad9b19177b2943751cd
YanceyOfficial <yanceyofficial@gmail.com>
2023-06-20 14:48:40 +0800

    feat: resolve rust selution for 2-add-two-numbers

--- src/leetcode/rust/src/medium/question_2.rs
+++ src/leetcode/rust/src/medium/question_2.rs
@@ -1,19 +1,16 @@
 // Definition for singly-linked list.
-// #[derive(PartialEq, Eq, Clone, Debug)]
-// pub struct ListNode {
-//   pub val: i32,
-//   pub next: Option<Box<ListNode>>
-// }
-//
-// impl ListNode {
-//   #[inline]
-//   fn new(val: i32) -> Self {
-//     ListNode {
-//       next: None,
-//       val
-//     }
-//   }
-// }
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
 
 #[allow(unused)]
 pub fn add_two_numbers(
