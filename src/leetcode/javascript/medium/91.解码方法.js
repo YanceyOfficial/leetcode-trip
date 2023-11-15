f9b14a0726964a75283137e6c5145344b45b6463
YanceyOfficial <yanceyofficial@gmail.com>
2023-11-15 19:41:58 +0800

    feat: finish 91-num-decodings

--- src/leetcode/javascript/medium/91.解码方法.js
+++ src/leetcode/javascript/medium/91.解码方法.js
@@ -9,5 +9,25 @@
  * @param {string} s
  * @return {number}
  */
-var numDecodings = function (s) {}
+var numDecodings = function (s) {
+  const n = s.length
+  const dp = new Array(n + 1).fill(0)
+  dp[0] = 1
+
+  for (let i = 1; i <= n; i++) {
+    if (s[i - 1] !== '0') {
+      dp[i] += dp[i - 1]
+    }
+
+    if (
+      i > 1 &&
+      s[i - 2] !== '0' &&
+      Number(s[i - 2]) * 10 + Number(s[i - 1]) <= 26
+    ) {
+      dp[i] += dp[i - 2]
+    }
+  }
+
+  return dp[n]
+}
 // @lc code=end
