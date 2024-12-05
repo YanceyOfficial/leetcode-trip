c03df8b4728ba07b3e8b6710c2decc056fdd8206
YanceyOfficial <yanceyofficial@gmail.com>
2024-12-05 19:41:48 +0800

    feat: update question 14

--- src/leetcode/rust/src/easy/question_14.rs
+++ src/leetcode/rust/src/easy/question_14.rs
@@ -1,14 +1,11 @@
 #[allow(unused)]
 pub fn longest_common_prefix(strs: Vec<String>) -> String {
-    let mut strs = strs;
-    strs.sort();
     let mut prefix: &str = &strs[0];
-    let mut has_found = false;
 
-    while !has_found {
-        has_found = strs.iter().all(|str| str.starts_with(prefix));
-
-        if !has_found {
+    loop {
+        if strs.iter().all(|str| str.starts_with(prefix)) {
+            break;
+        } else {
             prefix = &prefix[0..prefix.len() - 1];
         }
     }
