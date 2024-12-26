bcf60938a51915e3bef813d0cf6f9365ad18cafb
YanceyOfficial <yanceyofficial@gmail.com>
2024-12-26 18:56:04 +0800

    feat: update docs

--- src/leetcode/rust/src/main.rs
+++ src/leetcode/rust/src/main.rs
@@ -1,9 +1,9 @@
-mod easy;
-mod hard;
+// mod easy;
+// mod hard;
 mod medium;
 
 fn main() {
-    let res = medium::question_18::four_sum(vec![1, 0, -1, 0, -2, 2], 0);
+    let res = medium::question_93::restore_ip_addresses(String::from("25525511135"));
 
     println!("{:?}", res);
 }
--- src/leetcode/rust/src/medium/question_93.rs
+++ src/leetcode/rust/src/medium/question_93.rs
@@ -1,63 +1,48 @@
 #[allow(unused)]
 pub fn restore_ip_addresses(s: String) -> Vec<String> {
-    let mut res: Vec<String> = vec![];
-    let arr = s
-        .as_bytes()
-        .iter()
-        .enumerate()
-        .fold(vec![], |mut arr, (i, byte)| {
-            arr.push((byte - b'0') as i32);
-            arr
-        });
-
-    if arr.len() < 4 || arr.len() > 12 {
+    let n = s.len();
+    let mut res = Vec::new();
+    if n < 4 || n > 12 {
         return res;
     }
 
-    dfs(0, 0, &arr, &mut vec![0; 4], &mut res);
-
+    dfs(&s, 0, &mut Vec::new(), &mut res);
     res
 }
 
-fn dfs(
-    begin: usize,
-    segment_idx: usize,
-    arr: &Vec<i32>,
-    track: &mut Vec<i32>,
-    res: &mut Vec<String>,
-) {
-    if segment_idx == 4 {
-        if begin == arr.len() {
-            res.push(
-                track
-                    .iter()
-                    .map(|x| x.to_string())
-                    .collect::<Vec<_>>()
-                    .join("."),
-            );
-        }
+// Helper function to validate the parts of the IP address
+fn is_valid(part: &str) -> bool {
+    if part.len() > 1 && part.starts_with('0') {
+        return false;
+    }
+    if part.len() > 3 {
+        return false;
+    }
+    if part.parse::<i32>().unwrap() > 255 {
+        return false;
+    }
+    true
+}
 
+// Helper function to backtrack and restore IP addresses
+fn dfs(s: &str, begin: usize, track: &mut Vec<String>, res: &mut Vec<String>) {
+    if track.len() == 4 && begin == s.len() {
+        res.push(track.join("."));
+        return;
+    }
+    if track.len() == 4 {
         return;
     }
 
-    if begin == arr.len() {
-        return;
-    }
-
-    if arr[begin] == 0 {
-        track[segment_idx] = 0;
-        dfs(begin + 1, segment_idx + 1, arr, track, res);
-    }
-
-    let mut segment = 0;
-    for i in begin..arr.len() {
-        segment = segment * 10 + arr[i];
-
-        if segment > 0 && segment <= 255 {
-            track[segment_idx] = segment;
-            dfs(i + 1, segment_idx + 1, arr, track, res);
-        } else {
+    for i in 1..4 {
+        if begin + i > s.len() {
             break;
+        }
+        let part = &s[begin..begin + i];
+        if is_valid(part) {
+            track.push(part.to_string());
+            dfs(s, begin + i, track, res);
+            track.pop();
         }
     }
 }
