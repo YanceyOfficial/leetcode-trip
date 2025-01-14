1aebbdb80c95db4ac47844cba295adaecae9d5db
YanceyOfficial <yanceyofficial@gmail.com>
2025-01-14 18:50:18 +0800

    fix: update serveral problems

--- src/leetcode/rust/src/medium/question_438.rs
+++ src/leetcode/rust/src/medium/question_438.rs
@@ -2,47 +2,46 @@ use std::collections::HashMap;
 
 #[allow(unused)]
 pub fn find_anagrams(s: String, p: String) -> Vec<i32> {
-    let mut need_map: HashMap<u8, i32> = HashMap::new();
+    let mut need_map: HashMap<char, i32> = HashMap::new();
 
-    for letter in p.as_bytes() {
-        need_map.entry(*letter).and_modify(|e| *e += 1).or_insert(1);
+    for ch in p.chars() {
+        need_map.entry(ch).and_modify(|e| *e += 1).or_insert(1);
     }
 
     let mut res = Vec::new();
-    let need_map_len = need_map.len();
-    let mut meeted_count = 0;
-    let mut window_map: HashMap<u8, i32> = HashMap::new();
+    let mut met_count = 0;
+    let mut window_map: HashMap<char, i32> = HashMap::new();
     let (mut start, mut end) = (0, 0);
 
     while end < s.len() {
-        let end_letter = s.as_bytes()[end];
+        let end_ch = s.chars().nth(end).unwrap();
         end += 1;
 
-        if need_map.contains_key(&end_letter) {
+        if need_map.contains_key(&end_ch) {
             window_map
-                .entry(end_letter)
+                .entry(end_ch)
                 .and_modify(|e| *e += 1)
                 .or_insert(1);
 
-            if window_map.get(&end_letter) == need_map.get(&end_letter) {
-                meeted_count += 1;
+            if window_map.get(&end_ch) == need_map.get(&end_ch) {
+                met_count += 1;
             }
         }
 
         while end - start >= p.len() {
-            if need_map_len == meeted_count {
+            if need_map.len() == met_count {
                 res.push(start as i32);
             }
 
-            let start_letter = s.as_bytes()[start];
+            let start_ch = s.chars().nth(start).unwrap();
             start += 1;
 
-            if need_map.contains_key(&start_letter) {
-                if window_map.get(&start_letter) == need_map.get(&start_letter) {
-                    meeted_count -= 1;
+            if need_map.contains_key(&start_ch) {
+                if window_map.get(&start_ch) == need_map.get(&start_ch) {
+                    met_count -= 1;
                 }
 
-                window_map.entry(start_letter).and_modify(|e| *e -= 1);
+                window_map.entry(start_ch).and_modify(|e| *e -= 1);
             }
         }
     }
