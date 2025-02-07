09022f1c3af1758bcabbafced4df752e0c8fed0c
YanceyOfficial <yanceyofficial@gmail.com>
2025-02-07 19:57:50 +0800

    feat: update serveral problems

---
+++ src/leetcode/javascript/287.寻找重复数.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=287 lang=javascript
+ *
+ * [287] 寻找重复数
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  let slow = nums[0]
+  let fast = nums[nums[0]]
+  while (slow !== fast) {
+    slow = nums[slow]
+    fast = nums[nums[fast]]
+  }
+  slow = 0
+  while (slow !== fast) {
+    slow = nums[slow]
+    fast = nums[fast]
+  }
+  return slow
+}
+// @lc code=end
