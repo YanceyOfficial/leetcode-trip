522e91e61bbabc5cccefde42fee7a877188134ba
YanceyOfficial <yanceyofficial@gmail.com>
2025-02-24 22:08:27 +0800

    feat: update serveral tree problems

---
+++ src/leetcode/javascript/easy/637.二叉树的层平均值.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=637 lang=javascript
+ *
+ * [637] 二叉树的层平均值
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+    
+};
+// @lc code=end
+
