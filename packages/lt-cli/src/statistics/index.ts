5567b4efba4803e4341d2229fb64388d520c60cb
YanceyOfficial <yanceyofficial@gmail.com>
2025-02-25 02:57:15 +0800

    fix: fix typo

--- packages/lt-cli/src/statistics/index.ts
+++ packages/lt-cli/src/statistics/index.ts
@@ -4,13 +4,13 @@ import { javascriptPath, Category } from '../shared/constants'
 const easyCount = readdirSync(`${javascriptPath}/${Category.Easy}`).length
 const mediumCount = readdirSync(`${javascriptPath}/${Category.Medium}`).length
 const hardCount = readdirSync(`${javascriptPath}/${Category.Hard}`).length
-const totcal = easyCount + mediumCount + hardCount
+const total = easyCount + mediumCount + hardCount
 
 const statistics = [
   { category: Category.Easy, count: easyCount },
   { category: Category.Medium, count: mediumCount },
   { category: Category.Hard, count: hardCount },
-  { category: 'Total', count: totcal },
+  { category: 'Total', count: total },
 ]
 
 export const showStatistics = () => {
