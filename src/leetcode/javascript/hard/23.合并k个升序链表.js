287070f5e85595ecc0e77e22d929d8c9496c2d41
YanceyOfficial <yanceyofficial@gmail.com>
2025-11-24 18:00:02 +0800

    feat: update 23

--- src/leetcode/javascript/hard/23.合并k个升序链表.js
+++ src/leetcode/javascript/hard/23.合并k个升序链表.js
@@ -21,14 +21,14 @@ var mergeKLists = function (lists) {
     return null
   }
 
-  // 引入递归函数 merge，处理 lists[start] 到 lists[end] 范围内的合并
-  const merge = (listArray, start, end) => {
+  // 引入递归函数 merge, 处理 lists[start] 到 lists[end] 范围内的合并
+  const merge = (start, end) => {
     // 1. 基本情况 (Base Case)
-    // 当范围缩小到只有一个链表时，直接返回该链表
+    // 当范围缩小到只有一个链表时, 直接返回该链表
     if (start === end) {
-      return listArray[start]
+      return lists[start]
     }
-    // 当范围无效时（例如 start > end），返回 null
+    // 当范围无效时, 返回 null
     if (start > end) {
       return null
     }
@@ -37,10 +37,10 @@ var mergeKLists = function (lists) {
     const mid = Math.floor((start + end) / 2)
 
     // 递归合并左半部分
-    const left = merge(listArray, start, mid)
+    const left = merge(start, mid)
 
     // 递归合并右半部分
-    const right = merge(listArray, mid + 1, end)
+    const right = merge(mid + 1, end)
 
     // 3. 合并 (Conquer)
     // 将左右两部分的结果使用 mergeTwoLists 函数进行合并
@@ -48,11 +48,11 @@ var mergeKLists = function (lists) {
   }
 
   // 从整个链表数组的范围 [0, lists.length - 1] 开始递归
-  return merge(lists, 0, lists.length - 1)
+  return merge(0, lists.length - 1)
 }
 
 var mergeTwoLists = function (l1, l2) {
-  const dummy = new ListNode(-1)
+  const dummy = new ListNode()
   let curr = dummy
 
   while (l1 && l2) {
@@ -63,12 +63,11 @@ var mergeTwoLists = function (l1, l2) {
       curr.next = l2
       l2 = l2.next
     }
-
     curr = curr.next
   }
 
-  if (l1 !== null) curr.next = l1
-  if (l2 !== null) curr.next = l2
+  if (l1) curr.next = l1
+  if (l2) curr.next = l2
 
   return dummy.next
 }
